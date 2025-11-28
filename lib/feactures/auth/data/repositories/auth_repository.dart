c6465fbdabf4f8bf6d1ff6d202a789e3226910a5
anthonnyygpz <anthonyygpz@gmail.com>
2025-11-28 10:18:42 -0700

    refactor(auth_repository): change SignUp

--- lib/feactures/auth/data/repositories/auth_repository.dart
+++ lib/feactures/auth/data/repositories/auth_repository.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:substrack/feactures/auth/domain/entities/sign_in_entity.dart';
 import 'package:substrack/feactures/auth/domain/entities/sign_up_entity.dart';
+import 'package:substrack/feactures/auth/domain/entities/user_entity.dart';
 import 'package:substrack/feactures/auth/domain/repositories/iauth_repository.dart';
 
 class AuthRepository extends IAuthRepository {
@@ -21,21 +22,28 @@ class AuthRepository extends IAuthRepository {
   Stream<User?> get userChanges => _fireAuth.authStateChanges();
 
   @override
-  Future<void> signUp({required SignUpEntity newUser}) async {
+  Future<User?> signUp({required SignUpEntity newUser}) async {
     try {
+      final phothoUrl = newUser.photoUrl;
+
       final data = await _fireAuth.createUserWithEmailAndPassword(
         email: newUser.email,
         password: newUser.password,
       );
 
-      if (data.user != null) {
+      if (data.user == null) {
         throw Exception('No se encontro los datos del usuario.');
       }
 
-      data.user!.updateDisplayName(newUser.username);
-      if (newUser.photoUrl != null) {
-        data.user!.updatePhotoURL(newUser.photoUrl);
+      await data.user!.updateDisplayName(newUser.username);
+
+      if (phothoUrl != null && phothoUrl.isNotEmpty) {
+        await data.user!.updatePhotoURL(newUser.photoUrl);
       }
+
+      await data.user!.reload();
+
+      return _fireAuth.currentUser;
     } catch (e) {
       rethrow;
     }
@@ -57,6 +65,36 @@ class AuthRepository extends IAuthRepository {
   Future<void> signOut() async {
     try {
       await _fireAuth.signOut();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<User?> updateUser({required EditUserEntity user}) async {
+    try {
+      final password = user.password;
+      final photoUrl = user.photoUrl;
+      final username = user.username;
+
+      final currentUser = _fireAuth.currentUser;
+      if (currentUser == null) return null;
+
+      if (username != null && username.isNotEmpty) {
+        await currentUser.updateDisplayName(user.username);
+        print('Actualizo username');
+      }
+
+      if (photoUrl != null && photoUrl.isNotEmpty) {
+        await currentUser.updatePhotoURL(user.photoUrl);
+        print('Actualizo foto');
+      }
+
+      if (password != null && password.isNotEmpty) {
+        await currentUser.updatePassword(password);
+        print('Actualizo contraseña');
+      }
+      return _fireAuth.currentUser;
     } catch (e) {
       rethrow;
     }
