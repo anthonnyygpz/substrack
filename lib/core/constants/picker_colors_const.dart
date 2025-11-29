c2a5cbee53591e58deda2696af0529ac5a45333e
anthonnyygpz <anthonyygpz@gmail.com>
2025-11-29 13:52:57 -0700

    feat(subscriptions): add color picker and subscription options

---
+++ lib/core/constants/picker_colors_const.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+final List<Color> colors = [
+  const Color(0xFFEF4444),
+  const Color(0xFFF97316),
+  const Color(0xFFEAB308),
+  const Color(0xFF84CC16),
+  const Color(0xFF10B981),
+  const Color(0xFF06B6D4),
+  const Color(0xFF3B82F6),
+  const Color(0xFF6366F1),
+  const Color(0xFF8B5CF6),
+  const Color(0xFFD946EF),
+  const Color(0xFFE11D48),
+  const Color(0xFF0F172A),
+];
