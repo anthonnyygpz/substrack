62c4b8690ab67d9a9f291511677193abb9a205d5
anthonnyygpz <anthonyygpz@gmail.com>
2025-11-29 13:52:57 -0700

    feat(subscriptions): add color picker and subscription options

---
+++ lib/core/constants/subscription_options.dart
@@ -0,0 +1,11 @@
+final List<String> subscriptionOptions = [
+  'Streaming',
+  'Música',
+  'Gaming',
+  'Productividad',
+  'Nube y Almacenamiento',
+  'Gimnasio y Salud',
+  'Servicios',
+  'Educación',
+  'Otros',
+];
