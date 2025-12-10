c100ba23c2c97b56f2d52fd398ab8db7b07727c1
anthonnyygpz <anthonyygpz@gmail.com>
2025-12-10 13:08:57 -0700

    fix: fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,6 +15,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -43,6 +44,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
